e9f3966eca3a041c7d65a7431f41a6bb245e69c6
George <georgemullarm045@gmail.com>
2025-05-31 16:12:24 +0530

    Media queries were added

--- javascript.js
+++ javascript.js
@@ -14,11 +14,17 @@ document.getElementById('clickbutton').addEventListener('click',function(){
             // console.log('Name: '+data.location.name);
             // console.log('Temperature in C: '+data.current.temp_c);
             // console.log('Temperature in F: '+data.current.temp_f);
-            document.getElementById('result').innerHTML=`Celsius: ${data.current.temp_c} \n Fahrenheit ${data.current.temp_f} `          
+            document.getElementById('result').innerHTML=`Celsius: ${data.current.temp_c} \n Fahrenheit ${data.current.temp_f} `;
+             document.getElementById('result').style.backgroundColor = 'white';
+            document.getElementById('result').style.color = 'black';
+            document.getElementById('result').style.textShadow = 'none';
         })
         .catch(err => 
         {
             document.getElementById('result').innerHTML = 'Enter valid input!';
+            document.getElementById('result').style.backgroundColor = 'transparent';
+            document.getElementById('result').style.color = 'red';
+            document.getElementById('result').style.textShadow = '0px 0px 10px white';
         }
         )
     }
